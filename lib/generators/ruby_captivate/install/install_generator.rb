09815ef7239901ec5431004d5d2d3e054177d7cd
Leveton <mleveton@gmail.com>
2012-01-29 03:32:26 -0500

    25b

--- lib/generators/ruby_captivate/install/install_generator.rb
+++ lib/generators/ruby_captivate/install/install_generator.rb
@@ -3,8 +3,7 @@ module Ruby_Captivate
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../../../templates', __FILE__)
     class_option :template_engine
-    include Thor::Group
-    include Thor::Actions
+    require Thor
 
 
         copy_file 'ruby_captivate.rb', 'config/initializers/ruby_captivate.rb'
--- lib/generators/ruby_captivate/install/install_generator.rb~
+++ lib/generators/ruby_captivate/install/install_generator.rb~
@@ -3,7 +3,8 @@ module Ruby_Captivate
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../../../templates', __FILE__)
     class_option :template_engine
-    include Thor
+    include Thor::Group
+    include Thor::Actions
 
 
         copy_file 'ruby_captivate.rb', 'config/initializers/ruby_captivate.rb'
