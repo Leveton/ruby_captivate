5cfab08fb12725324815898adab4f1f221b9269d
Leveton <mleveton@gmail.com>
2012-01-29 00:50:16 -0500

    expand_paths

--- bin/install.rb
+++ bin/install.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 class InitializerGenerator < Rails::Generators::Base
   def create_initializer_file
     create_file "config/initializers/initializer.rb", "# Add initialization content here"
