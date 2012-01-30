aa33e79779f17730f01bce87a9f420e0abf02607
Leveton <mleveton@gmail.com>
2012-01-30 18:39:13 -0500

    34

--- lib/generators/ruby_captivate/install/install_generator.rb
+++ lib/generators/ruby_captivate/install/install_generator.rb
@@ -5,8 +5,13 @@ module Ruby_Captivate
       desc "Creates a ruby_captivate initializer and copies locale files to your application."
       class_option :orm
 
-     def add_my_initializer
-      template "ruby_captivate.rb", "config/initializers/ruby_captivate.rb"
-    end
+      def add_controller
+        template "captivate_results_controller.rb", "app/controllers/captivate_results_controller.rb"
+      end
+
+      def add_captivate_routes
+        captivate_route  = "match '/internalreport', :to => 'captivate_results#process_results'"
+        route captivate_route
+      end
     end
 end
